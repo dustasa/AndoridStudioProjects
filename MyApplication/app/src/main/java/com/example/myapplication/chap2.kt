ef5168e76d3586f802bc7d4f26fd40e98e992aff
sunao <dustasa@icloud.com>
2023-04-02 19:52:19 +0800

    使用 var 关键字定义的变量重新赋予相同类型的不同值

---
+++ MyApplication/app/src/main/java/com/example/myapplication/chap2.kt
@@ -0,0 +1,11 @@
+package com.example.myapplication
+
+fun main() {
+//    val favoriteActor = null
+//    val favoriteActor = "ZhangYi"
+
+    //使用 var 关键字定义的变量重新赋予相同类型的不同值
+    var favoriteActor = "ZhangYi"
+    favoriteActor = "ZhangSongwen"
+    println(favoriteActor)
+}
