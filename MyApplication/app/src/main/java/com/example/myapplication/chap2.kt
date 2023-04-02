e4da9d9bd09a5188c52ffc0a3d0bea85539c90d3
sunao <dustasa@icloud.com>
2023-04-02 19:53:40 +0800

    Null can not be a value of a non-null type String

--- MyApplication/app/src/main/java/com/example/myapplication/chap2.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/chap2.kt
@@ -5,7 +5,8 @@ fun main() {
 //    val favoriteActor = "ZhangYi"
 
     //使用 var 关键字定义的变量重新赋予相同类型的不同值
-    var favoriteActor = "ZhangYi"
-    favoriteActor = "ZhangSongwen"
+    var favoriteActor:String = "ZhangYi"
+    favoriteActor = null
+
     println(favoriteActor)
 }
