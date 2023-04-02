b4505b77bd52065ebc075fa24e5b254403a60528
sunao <dustasa@icloud.com>
2023-04-02 19:55:52 +0800

    在 Kotlin 中声明可为 null 的变量，您需要在相应类型的末尾添加 ? 运算符

--- MyApplication/app/src/main/java/com/example/myapplication/chap2.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/chap2.kt
@@ -6,8 +6,11 @@ fun main() {
 
     // 使用 var 关键字定义的变量重新赋予相同类型的不同值
     // String 数据属于不可为 null 类型，因此您无法为该变量重新赋予 null
-    var favoriteActor:String = "ZhangYi"
-    favoriteActor = null
+    // 在 Kotlin 中声明可为 null 的变量，您需要在相应类型的末尾添加 ? 运算符
+    var favoriteActor:String ?= "ZhangYi"
+    favoriteActor = "zhaojinmai"
+    println(favoriteActor)
 
+    favoriteActor = null
     println(favoriteActor)
 }
