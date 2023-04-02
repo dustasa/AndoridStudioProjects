a4b9b300c8fe4ffc9f929c2438ac41fd10205bbd
sunao <dustasa@icloud.com>
2023-04-02 20:02:36 +0800

    不允许直接引用 favoriteActor 变量的 length 属性，因为该变量有可能是null

--- MyApplication/app/src/main/java/com/example/myapplication/chap2.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/chap2.kt
@@ -7,9 +7,14 @@ fun main() {
     // 使用 var 关键字定义的变量重新赋予相同类型的不同值
     // String 数据属于不可为 null 类型，因此您无法为该变量重新赋予 null
     // 在 Kotlin 中声明可为 null 的变量，您需要在相应类型的末尾添加 ? 运算符
-    var favoriteActor:String ?= "ZhangYi"
+    var favoriteActor: String? = "ZhangYi"
     favoriteActor = "zhaojinmai"
     println(favoriteActor)
+    println(favoriteActor.length)
+
+    var favoriteActor2: String? = "Sandra Oh"
+    println(favoriteActor2.length)
+
 
     favoriteActor = null
     println(favoriteActor)
@@ -19,4 +24,6 @@ fun main() {
 
     number = 10
     println(number)
+
+
 }
