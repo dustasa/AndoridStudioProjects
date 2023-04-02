a51543e75cd727d5669c3a955dc091c73eab0562
sunao <dustasa@icloud.com>
2023-04-02 15:14:43 +0800

    更复杂的when条件

--- MyApplication/app/src/main/java/com/example/myapplication/test.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/test.kt
@@ -19,4 +19,13 @@ fun main() {
         "Green" -> println("go")
         else -> println("Invalid inputting")
     }
+
+    val x = 3
+    when(x) {
+        2 -> println("x is a prime number between 1 and 10.")
+        3 -> println("x is a prime number between 1 and 10.")
+        5 -> println("x is a prime number between 1 and 10.")
+        7 -> println("x is a prime number between 1 and 10.")
+        else -> println("x isn't a prime number between 1 and 10.")
+    }
 }
