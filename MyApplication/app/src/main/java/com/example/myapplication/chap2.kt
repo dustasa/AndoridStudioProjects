2587c0a8591b355c7e46e83807d8a1018a5f47e8
sunao <dustasa@icloud.com>
2023-04-02 19:54:13 +0800

    String 数据属于不可为 null 类型，因此您无法为该变量重新赋予 null

--- MyApplication/app/src/main/java/com/example/myapplication/chap2.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/chap2.kt
@@ -4,7 +4,8 @@ fun main() {
 //    val favoriteActor = null
 //    val favoriteActor = "ZhangYi"
 
-    //使用 var 关键字定义的变量重新赋予相同类型的不同值
+    // 使用 var 关键字定义的变量重新赋予相同类型的不同值
+    // String 数据属于不可为 null 类型，因此您无法为该变量重新赋予 null
     var favoriteActor:String = "ZhangYi"
     favoriteActor = null
 
